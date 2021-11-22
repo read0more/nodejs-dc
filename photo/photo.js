46ea0b16e8e9555775553146939ff0af60e05989
ykpark <read0more@gmail.com>
2021-11-22 21:53:56 +0900

    Validation, condition 수정

--- photo/photo.js
+++ photo/photo.js
@@ -1,26 +1,29 @@
 const fs = require("fs");
 var path = require("path");
 const args = process.argv.slice(2);
-const targetDir = path.join(__dirname, args[0]);
+const targetDir = path.join(__dirname, args[0] ?? "");
 const VIDEO_DIR = path.join(targetDir, "video");
 const CAPTURED_DIR = path.join(targetDir, "captured");
 const DUPLICATED_DIR = path.join(targetDir, "duplicated");
 
+if (!args[0] || !fs.existsSync(targetDir)) {
+  console.error("Please enter folder name.");
+  return;
+}
+
 fs.readdir(targetDir, (err, files) => {
   files.forEach((file) => {
-    isVideo(file) &&
+    if (isVideo(file)) {
       move(path.join(targetDir, file), path.join(VIDEO_DIR, file));
-
-    isCaptured(path.join(targetDir, file)) &&
+    } else if (isCaptured(file)) {
       move(path.join(targetDir, file), path.join(CAPTURED_DIR, file));
-
-    isDuplicated(file) &&
+    } else if (isDuplicated(file)) {
       move(path.join(targetDir, file), path.join(DUPLICATED_DIR, file));
+    }
   });
 });
 
 function isVideo(file) {
-  console.log(path.extname(file));
   return [".mp4", ".mov"].includes(path.extname(file));
 }
 
