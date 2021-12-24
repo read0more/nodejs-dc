ced0b792870290fe194b71f6b412012e3e9c2e9f
ykpark <read0more@gmail.com>
2021-12-24 21:56:41 +0900

    코딩 스타일 수정

--- server/5-error-handling/app.js
+++ server/5-error-handling/app.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import fs from 'fs';
-import fsAsync from 'fs/promises';
+import express from "express";
+import fs from "fs";
+import fsAsync from "fs/promises";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/file1', (req, res) => {
+app.get("/file1", (req, res) => {
   // 1.
   // fs.readFile('/file1.txt', (err, data) => {
   //   if (err) {
@@ -16,34 +16,34 @@ app.get('/file1', (req, res) => {
 
   // 2.
   try {
-    const data = fs.readFileSync('/file1.txt');
+    const data = fs.readFileSync("/file1.txt");
   } catch (error) {
     res.sendStatus(404);
   }
 });
 
-app.get('/file2', (req, res) => {
+app.get("/file2", (req, res) => {
   fsAsync
-    .readFile('/file2.txt') //
+    .readFile("/file2.txt") //
     .catch((error) => {
       res.sendStatus(404);
     });
 });
 
-app.get('/file3', async (req, res) => {
+app.get("/file3", async (req, res) => {
   try {
-    const data = await fsAsync.readFile('/file2.txt');
+    const data = await fsAsync.readFile("/file2.txt");
   } catch {
     res.sendStatus(404);
   }
 });
 
+// promise에 대한 에러처리를 따로 하지 않을 경우에는 아래의 콜백이 호출
 // 버전 5 이하에서는: require('express-async-errors');
-
-// Express 5 부터는 이렇게
+// Express 5 부터는 이렇게...
 app.use((error, req, res, next) => {
   console.error(error);
-  res.status(500).json({ message: 'Something went wrong' });
+  res.status(500).json({ message: "Something went wrong" });
 });
 
 app.listen(8080);
